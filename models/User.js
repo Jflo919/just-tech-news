6b9c9e94e9f180c84579273ee90846477d334c93
JulianF <floydjulian1991@gmail.com>
2022-04-23 16:25:17 -0700

    added table column definitions

--- models/User.js
+++ models/User.js
@@ -8,9 +8,47 @@ class User extends Model {}
 // define table columns and configuration
 User.init (
     {
+        // define an id column
+        id: {
+            // use the special Sequelize DataTypes object provide what type of data it is
+          type: DataTypes.INTEGER,
+          // this is the equivalent of SQL's `NOT NULL` option
+          allowNull: false,
+          // instruct that this is the Primary key
+          primaryKey: true,
+          // turn on auto increment
+          autoIncrement: true
+        },
+        // define a usernam column
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        // define an email column
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            // there cannot be any duplicate email values in this table
+            unique: true,
+            // if allowNull is set to false, we can run our data throught validators before creating the table data.
+            validate: {
+                isEmail: true
+            }
+        },
+        // define a password column
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                // the following means the password must be at least four characters long
+                len: [4]
+            }
+        }
+
         // TABLE COLUMN DEFINITIONS GO HERE
     },
     {
+        
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
 
         // pass in our imported sequelize connection (the direct connection to our database)
